f6e55082b534b1a09210f1e42e49e047150ce486
Oleg'Ease'Kharchuk <olegease@gmail.com>
2026-07-25 01:51:26 +0200

    Overglad 2K26-07-25 (#8)
    Reviewed-on: https://codeberg.org/soneight/gamelike/pulls/8

--- source/main.cxx
+++ source/main.cxx
@@ -1,15 +1,19 @@
 #include "app.hxx"
 // `son8`
 #include <son8/main.hxx>
-#include <glad/son8.hxx>
+// NOTE: to use `gl* and GL_* c-entities` define macro below
+#define SON8_OVERGLAD_INCLUDE_DEPRECATED
+#include <son8/overglad/v1_1.hxx>
 #include <glfw/son8.hxx>
-// `std`
+#include <son8/core/alias.hxx>
 #include <son8/cxx/atom.hxx> // thread
 #include <son8/cxx/file.hxx> // cerr
 #include <son8/cxx/flow.hxx> // exception
 // launcher
 void son8::main( APP_SKIP Args args ) try {
    APP_ASSERT_MSG( args.size( ) == 1, "argc must contain one argument" );
+   namespace gl = son8::overglad;
+   namespace gle = gl::enums;
    using namespace core;
    using namespace cxx::chrono_literals;
    using Cfg = windowed::Config;
@@ -29,9 +33,35 @@ void son8::main( APP_SKIP Args args ) try {
    });
 
    Thread draw{ [&window]( ) {
-      window.run_swap( []( ) {
-         glClearColor( .125f, .125f, .125f, 1.f );
-         glClear( GL_COLOR_BUFFER_BIT );
+      static constexpr Size Vertices_Count = 3;
+      static constexpr Size Vertices_Per = 2;
+      static constexpr Size Colors_Per = 3;
+      static constexpr Size Colors_Size = Colors_Per * Vertices_Count;
+      static constexpr Size Vertices_Size = Vertices_Count * Vertices_Per;
+      Flat< GLfloat, Vertices_Size > vertexData{{
+         0.f, 0.f,
+         1.f, 0.f,
+         .5f, 1.0f
+      }};
+      Flat< GLubyte, Colors_Size > colorData{{
+         255, 0, 0,
+         0, 255, 0,
+         0, 0, 255
+      }};
+
+      window.run_swap( [&verts = vertexData,&colors = colorData]( ) {
+         gl::clear_color( 0.125f );
+         gl::clear( gle::Clearbit::Color );
+         gl::load_identity( );
+         gl::ortho(-.2, 1.2,-.2, 1.2, -1., 1. );
+         gl::enable( gle::ClientState::Vertex );
+         gl::enable( gle::ClientState::Color );
+         gl::vertex_pointer( 2, GL_FLOAT, 0, verts.data( ) );
+         gl::color_pointer( 3, GL_UNSIGNED_BYTE, 0, colors.data( ) );
+         gl::draw_arrays( (GLenum)gle::Draw::Triangles, 0, Vertices_Count );
+         gl::disable( gle::ClientState::Color );
+         gl::disable( gle::ClientState::Vertex );
+         assert( not gl::get_error( ) && "app: gl get error triggered" );
       });
    }};
 
